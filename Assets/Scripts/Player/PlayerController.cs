26f9202129158b506704971519a5cfe223f80589
WhiteStorm <andrzejmmm1@gmail.com>
2024-12-15 20:46:53 +0100

    Add basic player controls
    Adjusted player starship position, scale, and input actions for locomotion.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,94 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] private InputActionAsset pcInputActions;
+        [SerializeField] private InputActionAsset xrInputActions;
+        [SerializeField] private bool isXR = true;
+        [SerializeField] private float speed = 1.0f;
         
+        private InputActionMap XRILeftHandMap;
+        private InputActionMap XRILeftHandInteractionMap;
+        private InputActionMap XRIRightHandMap;
+        private InputActionMap XRIRightHandInteractionMap;
+        private InputActionMap PCMap;
+
+        private void Awake()
+        {
+            if (isXR)
+            {
+                XRILeftHandMap = xrInputActions.FindActionMap("XRI Left");
+                XRILeftHandInteractionMap = xrInputActions.FindActionMap("XRI Left Interaction");
+                XRIRightHandMap = xrInputActions.FindActionMap("XRI Right");
+                XRIRightHandInteractionMap = xrInputActions.FindActionMap("XRI Right Interaction");
+                
+            }
+            else
+            {
+                PCMap = pcInputActions.FindActionMap("Player");
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (isXR)
+            {
+                XRILeftHandMap.Enable();
+                XRIRightHandMap.Enable();
+            }
+            else
+            {
+                PCMap.Enable();
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (isXR)
+            {
+                XRILeftHandMap.Disable();
+                XRIRightHandMap.Disable();
+            }
+            else
+            {
+                PCMap.Disable();
+            }
+        }
+
+        private void Update()
+        {
+            if (isXR)
+            {
+                Vector2 leftStick = XRILeftHandMap["Thumbstick"].ReadValue<Vector2>();
+                Vector2 rightStick = XRIRightHandMap["Thumbstick"].ReadValue<Vector2>();
+                Rotate(leftStick, rightStick);
+                float brakeVal = XRILeftHandInteractionMap["Select Value"].ReadValue<float>();
+                float gasVal = XRIRightHandInteractionMap["Select Value"].ReadValue<float>();
+                Move(gasVal - brakeVal);
+            }
+            else
+            {
+            }
+        }
+        
+        private void Move(float direction)
+        {
+            float moveSpeed = speed * direction * Time.deltaTime;
+            transform.position += transform.forward * moveSpeed;
+        }
+        
+        private void Rotate(Vector2 leftStick, Vector2 rightStick)
+        {
+            // Rotate the player based on the left stick
+            float pitch = -(leftStick.y + rightStick.y);
+            float yaw = rightStick.x;
+            float roll = -leftStick.x;
+            Vector3 rotation = new Vector3(pitch, yaw, roll);
+            transform.Rotate(rotation);
+        }
+
     }
 }
--- Assets/Scripts/Player/XRCameraController.cs
+++ Assets/Scripts/Player/XRCameraController.cs
@@ -6,34 +6,38 @@ namespace Player
 {
     public class XRCameraController : MonoBehaviour
     {
-        [Tooltip("Distance from the initial position to cancel xr movement")]
-        float distanceToCancel = 2;
+        [Tooltip("")] 
+        [SerializeField] private GameObject BoundarieSphere;
 
         private XROrigin xrOrigin;
         private Vector3 initialPosition;
         private Quaternion initialRotation;
-        
+        private float distanceToCancel = 1;
+
         void Start()
         {
             GameObject xRRig = transform.parent.transform.parent.gameObject;
-            if(xRRig == null)
+            if (xRRig == null)
                 return;
             xrOrigin = xRRig.GetComponent<XROrigin>();
             initialPosition = transform.position;
+            if (BoundarieSphere != null)
+                distanceToCancel = BoundarieSphere.GetComponent<SphereCollider>().radius;
         }
         
         void Update()
         {
-            if(xrOrigin == null)
+            if(xrOrigin == null || BoundarieSphere == null)
                 return;
             // Distance from initial position
+            initialPosition = BoundarieSphere.transform.position;
             float distance = Vector3.Distance(initialPosition, transform.position);
             // If the distance is greater than 2 cancel xr movement
             if (distance > distanceToCancel)
             {
                 Vector3 direction = (transform.position - initialPosition).normalized;
-                Vector3 new_location = initialPosition + direction * distanceToCancel;
-                xrOrigin.MoveCameraToWorldLocation(new_location);
+                Vector3 newLocation = initialPosition + direction * distanceToCancel;
+                xrOrigin.MoveCameraToWorldLocation(newLocation);
             }   
             
         }
